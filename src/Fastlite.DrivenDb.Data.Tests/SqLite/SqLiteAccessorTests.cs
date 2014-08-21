377d6663e63c02365fd8332c628dec2f2e985766
Bonewolf <adleatherwood@outlook.com>
2014-08-21 11:31:16 -0400

    icon update

--- src/Fastlite.DrivenDb.Data.Tests/SqLite/SqLiteAccessorTests.cs
+++ src/Fastlite.DrivenDb.Data.Tests/SqLite/SqLiteAccessorTests.cs
@@ -3,7 +3,7 @@ using Fastlite.DrivenDb.Data.Tests.Base.Infrastructure;
 using Fastlite.DrivenDb.Data.Tests.SqLite.Infrastructure;
 
 namespace Fastlite.DrivenDb.Data.Tests.SqLite
-{   
+{      
    public class SqLiteAccessorTests : DbAccessorTests
    {
       protected override IDbTestFixture CreateFixture()
