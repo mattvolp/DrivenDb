9909560022208bf43e510d8a1c876ad758a262a9
Fastlite <adleatherwood@gmail.com>
2014-09-05 23:56:46 -0400

    Changed signing for release only

--- src/Fastlite.DrivenDb/Properties/AssemblyInfo.cs
+++ src/Fastlite.DrivenDb/Properties/AssemblyInfo.cs
@@ -35,5 +35,4 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
 
-[assembly: InternalsVisibleTo("Fastlite.DrivenDb.Tests, PublicKey=00240000048000009400000006020000002400005253413100040000010001004b39b9d42d89997de44bc48e1476a3904b28032d39471b18a6a7a831a85fd4ef981df1b9af8c0480bfe067237078b5688f3e85c0a49cc8f289e1b378fc1389bb2eb9bedf6c7cf7a024b456703976da5a78cd0339d3ccb65677d3c2347541271d0ba2515f60a8b5410d36ffe822553c25b79be7130a44f0360d1748e9112b3589")]
-//[assembly: InternalsVisibleTo("Fastlite.DrivenDb.Tests")]
+[assembly: InternalsVisibleTo("Fastlite.DrivenDb.Tests")]
