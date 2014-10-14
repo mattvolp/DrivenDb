a02866ebb1ec5de8e30031634af9f105aa05ef26
Fastlite <adleatherwood@gmail.com>
2014-10-14 19:56:28 -0400

    mighty moose

---
+++ src/Moose.bat
@@ -0,0 +1,6 @@
+
+start "" "C:\Program Files (x86)\ContinuousTests\ContinuousTests.exe"
+
+REM start "" "D:\Files\Tony\Code\GitHub\ContinuousTests\ReleaseBinaries\ContinuousTests.exe"
+
+REM start "" "D:\Files\Tony\Code\GitHub\ContinuousTests\src\ContinuousTests\bin\Debug\ContinuousTests.exe"
