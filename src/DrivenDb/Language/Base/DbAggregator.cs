fdebd3e69e534342fed6e929f3912a7b1367aec1
Bonewolf <adleatherwood@outlook.com>
2014-11-14 08:09:28 -0500

    File header updates for consistency

--- src/DrivenDb/Language/Base/DbAggregator.cs
+++ src/DrivenDb/Language/Base/DbAggregator.cs
@@ -1,4 +1,16 @@
-﻿using System;
+﻿/**************************************************************************************
+ * Original Author : Anthony Leatherwood (adleatherwood@gmail.com)                              
+ * Source Location : https://github.com/Fastlite/DrivenDb    
+ *  
+ * This source is subject to the Microsoft Public License.
+ * Link: http://www.microsoft.com/en-us/openness/licenses.aspx
+ *  
+ * THIS CODE AND INFORMATION IS PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND, 
+ * EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED 
+ * WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A PARTICULAR PURPOSE. 
+ **************************************************************************************/
+
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
